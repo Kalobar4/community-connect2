1c69d3454a17a978813c08387d96fff690972526
Brad Evans <kaloba4r@gmail.com>
2019-09-10 11:12:28 -0400

    backup3

--- client/src/components/PostIt_Post/PostPostIt.js
+++ client/src/components/PostIt_Post/PostPostIt.js
@@ -1,30 +1,29 @@
-import React from "react";
-import MoreButton from "../MoreButton/MoreButton";
-import "./PostPostIt.css";
+import React from 'react';
+import MoreButton from '../MoreButton/MoreButton';
+import './PostPostIt.css';
+
+// Component to view and create new posts
 
 const PostPostIt = props => {
   const postImageArray = [
-    "https://www.stickpng.com/assets/images/58889250bc2fc2ef3a1860ab.png",
-    "https://www.stickpng.com/assets/images/588891f2bc2fc2ef3a1860a5.png"
+    'https://www.stickpng.com/assets/images/58889250bc2fc2ef3a1860ab.png',
+    'https://www.stickpng.com/assets/images/588891f2bc2fc2ef3a1860a5.png'
   ];
   const postImage =
     postImageArray[Math.floor(Math.random() * postImageArray.length)];
 
   return (
-    <div className="PostPostIt">
+    <div className='PostPostIt'>
       <div>
         <img
           src={postImage}
-          alt="url"
-          className="roundImg"
-          style={{ width: "60px " }}
+          alt='url'
+          className='roundImg'
+          style={{ width: '60px ' }}
         />
         <h3>{props.category}</h3>
         <h5>{props.post_title}</h5>
-        <MoreButton
-          type="posts"
-          id = {props._id}
-         />
+        <MoreButton type='posts' id={props._id} />
       </div>
       <div></div>
     </div>
